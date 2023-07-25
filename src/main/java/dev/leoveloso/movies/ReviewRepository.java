2b997a34706c0ec51ca1338e7565a617840d1eac
leandrovsk <contas.center@gmail.com>
2023-07-24 23:07:16 -0300

    feat: add basic spring api

---
+++ src/main/java/dev/leoveloso/movies/ReviewRepository.java
@@ -0,0 +1,9 @@
+package dev.leoveloso.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
